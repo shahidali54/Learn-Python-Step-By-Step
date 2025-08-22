6b9f36661cdc05fd9c0e58ed2b229d724843cbf4
Shahid Ali <shahidbaloch.sm@gmail.com>
2025-08-22 21:13:41 +0500

    Update sub_module1.py

---
+++ Basic/modules_and_packages/2_basics_of_packages/my_package/sub_package/sub_module1.py
@@ -0,0 +1,10 @@
+# my_package/sub_package/sub_module1.py
+
+def square(num):
+    """Return the square of a number."""
+    return num * num
+
+
+def greet_from_submodule1(name):
+    """Greet someone from sub_module1."""
+    return f"Hello {name}, greetings from sub_module1!"
